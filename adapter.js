480c384f8a63eabff4aef5b88c63383064d8088b
SAGEscobar <gsergioalfredo@yahoo.com>
2021-05-11 14:57:32 -0600

    Update adapter.js

--- adapter.js
+++ adapter.js
@@ -3,7 +3,7 @@ const FileSync = require("lowdb/adapters/FileSync");
 const Memory = require('lowdb/adapters/Memory')
 
 const json = require('./db.json')
-const isLocal = !process.env.NOW_REGION
+const isLocal = process.env.NOW_REGION
 const type = isLocal ? new FileSync('./db.json') : new Memory
 const db = low(type)
 
