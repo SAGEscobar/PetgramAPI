06aa85b4c362c6ef4658e0f8ac8005ba885090a2
SAGEscobar <gsergioalfredo@yahoo.com>
2021-05-11 15:11:53 -0600

    Update adapter.js

--- adapter.js
+++ adapter.js
@@ -2,6 +2,7 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const Memory = require('lowdb/adapters/Memory')
 
+//Obligando a sincronisar con el documento fisico
 const json = require('./db.json')
 const isLocal = true
 //process.env.NOW_REGION
