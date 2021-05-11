658ea7c66be650570f499bc05c83a17d6332469b
SAGEscobar <gsergioalfredo@yahoo.com>
2021-05-11 17:10:15 -0600

    Update adapter.js

--- adapter.js
+++ adapter.js
@@ -1,14 +1,12 @@
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
-const LocalStorage = require("lowdb/adapters/LocalStorage");
 const Memory = require('lowdb/adapters/Memory')
-const path = require('path');
 
 //Obligando a sincronisar con el documento fisico
 const json = require('./db.json')
 const isLocal = true
 //process.env.NOW_REGION
-const type = new FileSync("https://my-json-server.typicode.com/SAGEscobar/petgramDB/db")
+const type = new Memory
 
 const db = low(type)
 db.defaults(json).write()
