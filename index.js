dd8a2319276ca10369cfe1b83373261580c0fdd6
SAGEscobar <gsergioalfredo@yahoo.com>
2021-05-11 15:18:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const { ApolloServer } = require('apollo-server-express')
 const { resolvers, typeDefs } = require('./schema')
 const jwt = require('express-jwt')
 
-
-// this is not secure! this is for dev purposes
 process.env.JWT_SECRET = process.env.JWT_SECRET || 'somereallylongsecretkey'
-
 
 const PORT = process.env.PORT || 3500
 const app = express()
